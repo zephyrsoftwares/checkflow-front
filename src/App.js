7711cbde92c89558cac6231bddd2f0c6013c03d7
fAlcide <alcide.faucheron@etu.uphf.fr>
2023-11-08 21:50:20 +0100

    Create firebase connection, modifying index viex and adding pacakge-lock.json to gitignore

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import logo from "./assets/images/logo.svg";
 import text from "./assets/images/text.svg";
+
+import { db } from "./config/firebase";
+import {
+  getDocs,
+  collection,
+  onSnapshot,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
 
 import "./assets/styles/App.css";
 
 function App() {
+  const [structureList, setStructureList] = useState([]);
+
+  const structureCollectionRef = collection(db, "structure");
+
+  const getStructureList = () => {
+    const unsubscribe = onSnapshot(structureCollectionRef, (querySnapshot) => {
+      const updatedStructureList = [];
+      querySnapshot.forEach((doc) => {
+        updatedStructureList.push({ ...doc.data(), id: doc.id });
+      });
+      setStructureList(updatedStructureList);
+    });
+
+    return unsubscribe;
+  };
+
+  useEffect(() => {
+    const unsubscribe = getStructureList();
+
+    //show all items in structureList
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} width="350" className="App-logo mb-9" alt="logo" />
         <img src={text} width="350" alt="Site en construction." />
+        <h3 className="text-black">Structures :</h3>
       </header>
+      <div className="App-body">
+        {structureList.map((structure) => (
+          <div key={structure.id} className="structure">
+            <div className="">{structure.title}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,22 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Firebase configuration variables
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGIN_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY_DEV,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN_DEV,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID_DEV,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET_DEV,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID_DEV,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID_DEV,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Export Firebase modules
-export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
