a13a5a61d940b61a623ebb6ef089411ee9279bb1
Ibrahim ADLANI <ibrahim@adlani.com>
2023-11-09 12:34:05 +0100

    Commented ActivityList

--- src/components/activitylist.js
+++ src/components/activitylist.js
@@ -20,26 +20,33 @@ const activityItems = [
     projectName: "PESR Fin d'année",
     commit: "Audit IT",
     branch: "Bain & Co",
-    date: "1h",
-    dateTime: "2023-01-23T11:00",
+    date: "3h",
+    dateTime: "2023-01-23T09:00",
   },
-  // More items...
 ];
 
 const ActivityList = () => {
   return (
+    // LISTE D'ACTIVITÉS
     <ul role="list" className="divide-y divide-white/5">
       {activityItems.map((item) => (
+        // ACTIVITÉ
         <li key={item.commit} className="px-4 py-4 sm:px-6 lg:px-8">
+          {/* LIGNE SUPERIEURE */}
           <div className="flex items-center gap-x-3">
+            {/* AVATAR */}
             <img
               src={item.user.imageUrl}
               alt=""
               className="h-6 w-6 flex-none rounded-full bg-gray-800"
             />
+
+            {/* NOM */}
             <h3 className="flex-auto truncate text-sm font-semibold leading-6 text-white">
               {item.user.name}
             </h3>
+
+            {/* TIME */}
             <time
               dateTime={item.dateTime}
               className="flex-none text-xs text-gray-600"
@@ -47,10 +54,17 @@ const ActivityList = () => {
               {item.date}
             </time>
           </div>
+
+          {/* LIGNE INFERIEURE */}
           <p className="mt-3 truncate text-sm text-gray-500">
-            Modif. sur <span className="text-gray-400">{item.projectName}</span>{" "}
-            (<span className="font-mono text-gray-400">{item.commit}</span> on{" "}
-            <span className="text-gray-400">{item.branch}</span>)
+            {/* ACTION */}
+            Modif. sur <span className="text-gray-400">
+              {item.projectName}
+            </span>{" "}
+            {/* TARGET */}
+            <span className="font-mono text-gray-400">
+              {item.commit}
+            </span> on <span className="text-gray-400">{item.branch}</span>
           </p>
         </li>
       ))}
